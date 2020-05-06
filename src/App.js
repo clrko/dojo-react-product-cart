fdee30ac3e30a90f8d3efb3227392013355857d0
Claire Kodia <claire.kodia@gmail.com>
2020-05-06 11:36:41 +0200

    Step 5 with the from

--- src/App.js
+++ src/App.js
@@ -10,25 +10,48 @@ const initialProductList = [
 const App = () => {
   const [productList, setProductList] = useState(initialProductList)
   const [totalPriceList, setTotalPriceList] = useState(initialProductList.map(product => product.price * product.quantity))
-  
-  const addQuantity = e => {
-    if (e.target.value === "0" || e.target.value === "") {
-      if (window.confirm("Etes-vous sûr de bien vouloir retirer ce produit de la liste ?")) {
-        let arr = [...productList]
-        let row = arr.find(f => f.id === parseInt(e.target.id))
-        arr = arr.filter(f => f !== row)
-        setProductList(arr)
+  const [newProduct, setNewProduct] = useState({id:"", name: '', price:'', quantity:1 })
 
-      } else {
-        setProductList(productList)
+  const addQuantity = e => {
+      if (e.target.value === "0" || e.target.value === "") {
+        if (window.confirm("Etes-vous sûr de bien vouloir retirer ce produit de la liste ?")) {
+          let arr = [...productList]
+          let row = arr.find(f => f.id === parseInt(e.target.id))
+          arr = arr.filter(f => f !== row)
+          setProductList(arr)
+
+        } else {
+          setProductList(productList)
+        } 
+      } else if (e.target.value !== "") {
+      let newArr = [...productList]
+      newArr[e.target.id -1].quantity = e.target.value
+      setProductList(newArr)
+      setTotalPriceList(newArr.map(product => product.price * product.quantity))
       }
-    } else if (e.target.value !== ""){
-    let newArr = [...productList]
-    newArr[e.target.id -1].quantity = e.target.value
-    setProductList(newArr)
-    setTotalPriceList(newArr.map(product => product.price * product.quantity))
+  }
+
+  const addProduct = e => {
+    let tempIndex = Math.floor(Math.random() * 10) + 1
+    while (productList.map(product => product.id).includes(tempIndex)) {
+      tempIndex = Math.floor(Math.random() * 10)
+    } 
+    setNewProduct({...newProduct, id: tempIndex})
+    e.preventDefault();
+    const newArray = [...productList]
+    newArray.push(newProduct)
+    setProductList(newArray)
+  }
+
+  const onChange = e => {
+    if (e.target.name === 'name') {
+      setNewProduct({...newProduct, name: e.target.value})
+    } else if (e.target.name === 'price') {
+      setNewProduct({...newProduct, price: parseInt(e.target.value)})
     }
   }
+
+
   
   return (
     <div className='App'>
@@ -52,8 +75,23 @@ const App = () => {
           </tr>)}
         </tbody>
       </table>
-          <p>Montant de la commande: {totalPriceList.reduce((accumulator, currentValue) => accumulator + currentValue
-)}</p>
+          <p>Montant de la commande: 
+          <span style={{ fontWeight: 'bold' }}> {totalPriceList.reduce((accumulator, currentValue) => accumulator + currentValue
+)}€</span></p>
+      
+      <form onSubmit={addProduct}>
+        <h2>Ajouter un produit</h2>
+        <div className="field">
+          <label htmlFor="name">Nom</label>
+          <input type='text' id="name" name="name" onChange={onChange} value={newProduct.name} ></input>
+        </div>
+        <div className="field">
+          <label htmlFor="price" >Prix</label>
+          <input type='number' id="price" name="price" onChange={onChange} value={newProduct.price} ></input>
+        </div>
+        <button>Ajouter</button>
+      </form>
+
     </div>
   );
 }
